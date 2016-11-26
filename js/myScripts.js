d22bc164e1257a05064ce5c86a3419c58bb5d431
TomZoy <mr.tomzoy@gmail.com>
2016-11-26 19:22:06 +0000

    working on the main page

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,4 +1,14 @@
-﻿
+﻿/* declare global variables */
+
+//keyTerms
+var isKeytermsLoaded = false;
+var keyTerms = []; //variable to hold the objects
+var museums = []; //variable to hold museum objects
+
+
+
+/* custom scripts */
+
 //function to randoml select the hero image and load it when the main page loads
 function loadHeroImage() {
     //initialise variables
@@ -22,3 +32,69 @@ $("#heroImage").ready(function() {
 });
 
 
+
+//function to parse searchTerms from php
+function loadKeyTerms() {
+
+    //get the JSON from the php file, and store it to an array
+    $.getJSON("keyterms.php", function (data) {
+
+        $.each(data, function (key, val) {
+
+            $.each(val, function (key2, val2) {
+                var keyTerm = new Object();
+                keyTerm.id = val2.term_id;
+                keyTerm.term = val2.term;
+                keyTerms.push(keyTerm);
+            });
+        });
+
+        isKeytermsLoaded = true;
+
+    });
+
+}
+
+
+
+//function to parse museums from php
+function loadMuseums() {
+
+    //get the JSON from the php file, and store it to an array
+    $.getJSON("museums.php", function (data) {
+
+        console.log(data);
+        return data;
+
+    });
+
+}
+
+
+// function to drive the search-bar
+$(document).ready(function () {
+    $('input.autocomplete').autocomplete({
+            data: {
+                "Apple": null,
+                "Microsoft": null,
+                "Google": 'http://placehold.it/250x250'
+            }
+        });
+});
+
+// animate the main searchbar
+$(document).ready(function () {
+    var sb = $("#searchBarWrapper");
+    sb.focusin(function () {
+        if (sb.hasClass("m4")) {
+            sb.removeClass("m4 offset-m4");
+            sb.addClass("m8 offset-m2");
+        }
+    });
+    sb.focusout(function () {
+        if (sb.hasClass("m8")) {
+            sb.removeClass("m8 offset-m2");
+            sb.addClass("m4 offset-m4");
+        }
+    });
+});
