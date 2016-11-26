c18c2b43aea225e750556cdefa84c91179676914
TomZoy <mr.tomzoy@gmail.com>
2016-11-26 17:37:36 +0000

    restarting the project + making some progress

---
+++ js/myScripts.js
@@ -0,0 +1,24 @@
+﻿
+//function to randoml select the hero image and load it when the main page loads
+function loadHeroImage() {
+    //initialise variables
+    var heroImagePrefix = "images/hero/";
+    var heroImageArray = [
+        "pexels-photo-135018.jpeg",
+        "pexels-photo-135019.jpeg",
+        "pexels-photo-457896.jpg",
+        "pexels-photo-457989.jpg",
+        "pexels-photo-585995.jpg"
+    ];
+
+    //set the hero image randomly
+    var i = Math.floor((Math.random() * heroImageArray.length));
+    $("#heroImage").css("background-image", "url('" + heroImagePrefix + heroImageArray[i] + "')");
+
+    console.log("url('" + heroImagePrefix + heroImageArray[i] + "')");
+};
+$("#heroImage").ready(function() {
+    loadHeroImage();
+});
+
+
